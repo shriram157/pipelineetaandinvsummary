408e05e8e6d863e388332294f336db03447ccd45
S0019881969 <aarti_dhamat@toyota.ca>
2018-11-26 13:02:33 +0000

    committing changes - testing data Service locally+node

--- pipelineInventory/webapp/controller/master.controller.js
+++ pipelineInventory/webapp/controller/master.controller.js
@@ -10,29 +10,27 @@ sap.ui.define([
 		/*Initialization of the page data*/
 		onInit: function () {
 			_that = this;
-			/*Fetching data from Dealer Service */
-			$.ajax({
-				dataType: "json",
-				url: "/node/API_BUSINESS_PARTNER/",
-				type: "GET",
-				success: function (oData) {
-					// debugger;
-					var dealerObj = {
-						DealerList: []
-					};
-					$.each(oData.d.results, function (i, item) {
-						var DataLength = item.BusinessPartner.length;
-						dealerObj.DealerList.push({
-							"BusinessPartner": item.BusinessPartner.substring(5, DataLength),
-							"BusinessPartnerName": item.OrganizationBPName1 //item.BusinessPartnerFullName
-						});
-					});
-					_that.oGlobalJSONModel.getData().DealerList = dealerObj.DealerList;
-					_that.oGlobalJSONModel.updateBindings();
-					// _that.getView().byId("multiheader").setHeaderSpan([3, 2, 1]);
-				},
-				error: function (oError) {}
+			
+			_that.oI18nModel = new sap.ui.model.resource.ResourceModel({
+				bundleUrl: "i18n/i18n.properties"
 			});
+			_that.getView().setModel(_that.oI18nModel, "i18n");
+
+			if (window.location.search == "?language=fr") {
+				_that.oI18nModel = new sap.ui.model.resource.ResourceModel({
+					bundleUrl: "i18n/i18n.properties",
+					bundleLocale: ("fr")
+				});
+				_that.getView().setModel(_that.oI18nModel, "i18n");
+				_that.sCurrentLocale = 'FR';
+			} else {
+				_that.oI18nModel = new sap.ui.model.resource.ResourceModel({
+					bundleUrl: "i18n/i18n.properties",
+					bundleLocale: ("en")
+				});
+				_that.getView().setModel(_that.oI18nModel, "i18n");
+				_that.sCurrentLocale = 'EN';
+			}
 			/*Global Model initialization and mapping on view*/
 			_that.oGlobalJSONModel = new JSONModel();
 			_that.getView().setModel(_that.oGlobalJSONModel, "GlobalJSONModel");
@@ -41,6 +39,7 @@ sap.ui.define([
 			_that.oGlobalJSONModel.getData().suffixData = [];
 			_that.oGlobalJSONModel.getData().DealerList = [];
 			_that.oGlobalJSONModel.getData().ModelYearList = [];
+			
 			/*Getting Year dropdown data*/
 			_that.modelYearPicker = this.getView().byId("ID_modelYearPicker");
 			_that.currentYear = new Date().getFullYear();
@@ -61,10 +60,46 @@ sap.ui.define([
 			_that.oGlobalJSONModel.getData().ModelYearList = _ObjModelYear.ModelYearList;
 			_that.oGlobalJSONModel.updateBindings();
 			_that.modelYearPicker.setSelectedKey(_that.currentYear);
-			_that.oDataService = "/node";
+			
+			
+			// _that.oDataService = "/node";
+			var sLocation = window.location.host;
+			var sLocation_conf = sLocation.search("webide");
+
+			if (sLocation_conf == 0) {
+				this.sPrefix = "/pipelineInventory-dest";
+			} else {
+				this.sPrefix = "";
+			}
+			this.nodeJsUrl = this.sPrefix + "/node";
+			
+			/*Fetching data from Dealer Service */
 			$.ajax({
 				dataType: "json",
-				url: "/node/Z_VEHICLE_CATALOGUE_MODEL/",
+				url: this.nodeJsUrl + "/API_BUSINESS_PARTNER/",
+				type: "GET",
+				success: function (oData) {
+					// debugger;
+					var dealerObj = {
+						DealerList: []
+					};
+					$.each(oData.d.results, function (i, item) {
+						var DataLength = item.BusinessPartner.length;
+						dealerObj.DealerList.push({
+							"BusinessPartner": item.BusinessPartner.substring(5, DataLength),
+							"BusinessPartnerName": item.OrganizationBPName1 //item.BusinessPartnerFullName
+						});
+					});
+					_that.oGlobalJSONModel.getData().DealerList = dealerObj.DealerList;
+					_that.oGlobalJSONModel.updateBindings();
+					// _that.getView().byId("multiheader").setHeaderSpan([3, 2, 1]);
+				},
+				error: function (oError) {}
+			});
+			
+			$.ajax({
+				dataType: "json",
+				url: this.nodeJsUrl + "/Z_VEHICLE_CATALOGUE_SRV/zc_model",
 				type: "GET",
 				success: function (oData) {
 					console.log("Ajax data", oData.d.results);
@@ -75,7 +110,7 @@ sap.ui.define([
 			});
 			$.ajax({
 				dataType: "json",
-				url: "/node/Z_VEHICLE_CATALOGUE_MM/",
+				url: this.nodeJsUrl + "/Z_VEHICLE_CATALOGUE_SRV/zc_mmfields",
 				type: "GET",
 				success: function (oData) {
 					console.log("Ajax data", oData.d.results);
@@ -87,7 +122,7 @@ sap.ui.define([
 
 			$.ajax({
 				dataType: "json",
-				url: "/node/Z_VEHICLE_CATALOGUE_TRIM/",
+				url: this.nodeJsUrl +"/Z_VEHICLE_CATALOGUE_SRV/zc_exterior_trim",
 				type: "GET",
 				success: function (oData) {
 					console.log("Ajax data", oData.d.results);
@@ -96,47 +131,8 @@ sap.ui.define([
 				},
 				error: function (oError) {}
 			});
-			// _that.oDataServiceModel = new sap.ui.model.odata.v2.ODataModel(_that.oDataService);
-			// _that.oDataServiceModel.setUseBatch(true);
-			// var oData1;
-			// _that.oDataServiceModel.read("/Z_VEHICLE_CATALOGUE_MODEL ", true);
-			// _that.oDataServiceModel.attachRequestCompleted(function (oEvent) {
-			// 	Data1 = oEvent.getSource().oData;
-			// 	// _that.oGlobalJSONModel.getData().modelData = oEvent.getSource().oData.results;
-			// 	console.log("V2 data", oData1);
-			// });
-			// _that.oDataServiceModel = new sap.ui.model.odata.ODataModel(_that.oDataService);
-			// _that.oDataServiceModel.read("/Z_VEHICLE_CATALOGUE_MODEL", null, null, true,
-			// 	function (oData) {
-			// 		console.log("zc_model", oData);
-			// 		_that.oGlobalJSONModel.getData().modelData = oData.results;
-			// 		_that.oGlobalJSONModel.updateBindings();
-			// 	},
-			// 	function (oError) {});
-			// _that.oDataServiceModel = new sap.ui.model.odata.ODataModel(_that.oDataService);
-			// _that.oDataServiceModel.read("/Z_VEHICLE_CATALOGUE_MM", null, null, true,
-			// 	function (oData) {
-			// 		console.log("zc_MMFields", oData);
-			// 		_that.oGlobalJSONModel.getData().seriesData = oData.results;
-			// 		_that.oGlobalJSONModel.updateBindings();
-			// 	},
-			// 	function (oError) {});
-			// _that.oDataServiceModel.read("Z_VEHICLE_CATALOGUE_TRIM", null, null, true,
-			// 	function (oData) {
-			// 		console.log("zc_exterior_trim", oData);
-			// 		_that.oGlobalJSONModel.getData().suffixData = oData.results;
-			// 		_that.oGlobalJSONModel.updateBindings();
-			// 	},
-			// 	function (oError) {});
-			/* MAIN SERVICE https://tcid1gwapp1.tci.internal.toyota.ca:44300/sap/opu/odata/sap/Z_VEHICLE_MASTER_SRV/ */
-			/*EntitySet for pulling series data- zc_mmfields*/
-			/*EntitySet for pulling Model data- zc_model*/
-			/*EntitySet for pulling series data- zc_exterior_trim*/
-			/*for dealer list: https://tcipn1sap01.tci.internal.toyota.ca:51129/API_BUSINESS_PARTNER*/
-			var i18nModel = new ResourceModel({
-				bundleName: "toyota.ca.xsaapp.PipelineETAInventSummary.i18n.i18n"
-			});
-			_that.getView().setModel(i18nModel, "i18nModel");
+			
+
 			_that.oSelectJSONModel = new JSONModel();
 			_that.getView().setModel(_that.oSelectJSONModel, "SelectJSONModel");
 			sap.ui.getCore().setModel(_that.oSelectJSONModel, "SelectJSONModel");
@@ -216,46 +212,37 @@ sap.ui.define([
 			_that.getView().byId("tableMultiHeader3").getColumns()[10].setHeaderSpan([7, 4, 1]);
 			_that.getView().byId("tableMultiHeader3").getColumns()[15].setHeaderSpan([2, 2, 1]);
 		},
+		
 		/*Fetch data on apply filter click for all three tables*/
 		applyFiltersBtn: function () {
 			_that.oSelectJSONModel.setData(_that.objList);
 			_that.oSelectJSONModel.updateBindings();
 		},
-		// click: function () {
-		// 	_that.getView().byId("multiheaderColumn").setHeaderSpan([3, 2, 1]);
-		// },
+		
 		/*For Switching the Pages*/
 		selectedScreen: function (oSelectedScreen) {
 			var selectedScreenText = oSelectedScreen.getParameters().selectedItem.getText();
 			if (selectedScreenText == "Master") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Pipeline ETA & Inventory Summary");
 				_that.getRouter().navTo("Routemaster");
 			} else if (selectedScreenText == "Details") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Details");
 				_that.getRouter().navTo("details");
 			} else if (selectedScreenText == "Vehicle Details") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Vehicle Details");
 				_that.getRouter().navTo("vehicleDetails");
 			} else if (selectedScreenText == "Order Change") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Order Change");
 				_that.getRouter().navTo("orderChange");
 			} else if (selectedScreenText == "Ship To Dealer") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Ship To Dealer");
 				_that.getRouter().navTo("shipToDealer");
 			} else if (selectedScreenText == "Ship To Dealer Response") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Ship To Dealer Response");
 				_that.getRouter().navTo("shipToDealerResponse");
 			} else if (selectedScreenText == "Assign Vehicles") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Assign Vehicles");
 				_that.getRouter().navTo("assignVehicles");
 			} else if (selectedScreenText == "Assign Vehicles Status") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Assign Vehicles Status");
 				_that.getRouter().navTo("assignVehiclesStatus");
 			} else if (selectedScreenText == "Change History") {
-				// oSelectedScreen.getSource().getParent().getContentLeft()[2].setText("Change History");
 				_that.getRouter().navTo("changeHistory");
 			}
 		},
+		
 		/*Funtion to get the changed year*/
 		handleYearChange: function (oYearEvent) {
 			// oYearEvent.getSource().setMinDate(new Date("2017"));
--- pipelineInventory_node/myRouter/myRoutes/myNode.js
+++ pipelineInventory_node/myRouter/myRoutes/myNode.js
@@ -8,43 +8,16 @@ var passport = require('passport');
 // var JWTStrategy = require('@sap/xssec').JWTStrategy; 
 
 var async = require('async');
-
 module.exports = function () {
 	var app = express.Router();
 	//Hello Router
 	app.get('/', (req, res) => {
 		var output = '<a os Details</a> - Your Node Module is up and Running</br> ';
-		// console.log(req);
-
 		res.type('text/html').status(200).send(output);
 	});
-
-	app.get('/os', (req, res) => {
-		var os = require('os');
-		var output = {};
-
-		output.tmpdir = os.tmpdir();
-		output.endianness = os.endianness();
-		output.hostname = os.hostname();
-		output.type = os.type();
-		output.platform = os.platform();
-		output.arch = os.arch();
-		output.release = os.release();
-		output.uptime = os.uptime();
-		output.loadavg = os.loadavg();
-		output.totalmem = os.totalmem();
-		output.freemem = os.freemem();
-		output.cpus = os.cpus();
-		output.networkInfraces = os.networkInterfaces();
-
-		var result = JSON.stringify(output);
-		res.type('application/json').status(200).send(result);
-	});
-
 	var auth64;
-
+	
 	// SAP Calls Start from here
-
 	var options = {};
 	options = Object.assign(options, xsenv.getServices({
 		api: {
@@ -52,177 +25,67 @@ module.exports = function () {
 		}
 	}));
 
+
 	var uname = options.api.user,
 		pwd = options.api.password,
 		url = options.api.host,
 		APIKey = options.api.APIKey,
 		client = options.api.client;
+     	console.log('The API Management URL', url);
+        
+	var auth64 = 'Basic ' + new Buffer(uname + ':' + pwd).toString('base64');
 
-	console.log(options);
-
-	auth64 = 'Basic ' + new Buffer(uname + ':' + pwd).toString('base64');
+	var reqHeader = {
+		"Authorization": auth64,
+		"Content-Type": "application/json",
+		"APIKey": APIKey,
+		"x-csrf-token": "Fetch"
+	};
 
 	app.use(function (req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		next();
 	});
+	
+	// simple version of bridge call
+	app.all('/*', function(req, res, next) {
 
-	// app.all('/*', function (req, res, next) {
-	// 	console.log("request", req);
-	// 	let headOptions = {};
-	// 	var csrfToken;
-	// 	// prepare the magic reverse proxy header for odata service to work. 
-	// 	let originalHost = req.hostname;
-	// 	if (!!req.headers.host) {
-	// 		headOptions.host = req.headers.host;
-	// 	}
+    	let headOptions = {};
+			var csrfToken;
+		// prepare the magic reverse proxy header for odata service to work. 
+        // let originalHost = req.hostname;
+        // if (!!req.headers.host){
+        //     headOptions.host = req.headers.host;
+        // }
 
-	// 	//only support the basic auth
-	// 	headOptions.Authorization = auth64;
+		//only support the basic auth
+        headOptions.Authorization = auth64;
 
-	// 	let method = req.method;
-	// 	console.log("requested url",req.url);
-	// 	let xurl = url + req.url;
-	// 	console.log('Method', method);
-	// 	console.log('URL that I am running', xurl);
-	// 	let xRequest =
-	// 		request({
-	// 			method: method,
-	// 			url: xurl,
-	// 			headers: headOptions
-	// 		});
-	// 	req.pipe(xRequest);
-	// 	xRequest.on('response', (response) => {
-	// 		// delete response.headers['set-cookie'];
-	// 		csrfToken = response.headers['x-csrf-token'];
-	// 		xRequest.pipe(res);
-	// 	}).on('error', (error) => {
-	// 		next(error);
-	// 	});
-	// });
+        let method = req.method;
+        let xurl = url +req.url;
+         console.log('Method', method);
+        console.log('Incoming Url', xurl);
+        let xRequest = 
+			request({
+				method : method,
+				url : xurl,
+				headers: reqHeader
+ 			}
+		);
+        
+        req.pipe(xRequest);
 
-	//api business partner call
-	app.get('/API_BUSINESS_PARTNER', function (req, res) {
-
-		console.log("inside the API_BUSINESS_PARTNER")
-
-		console.log("inside zvehicle master");
-		console.log(url);
-		console.log(url + "/API_BUSINESS_PARTNER/");
-
-		var csrfToken;
-		request({
-			url: url +
-				"/API_BUSINESS_PARTNER/A_BusinessPartner?$format=json&?sap-client=" + client +
-				"&$filter=BusinessPartnerType%20eq%20%27Z001%27&$orderby=BusinessPartnerName",
-			headers: {
-				"Authorization": auth64,
-				"Content-Type": "application/json",
-				"APIKey": APIKey,
-				"x-csrf-token": "Fetch"
-			}
-
-		}, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				csrfToken = response.headers['x-csrf-token'];
-				// console.log(csrfToken);
-				var json = JSON.parse(body);
-				res.json(json);
-			} else {
-
-				var result = JSON.stringify(body);
-				res.type('application/json').status(401).send(result);
-			}
-		});
+        xRequest.on('response', (response) => {
+        	// delete response.headers['set-cookie'];
+        		csrfToken = response.headers['x-csrf-token'];
+        	xRequest.pipe(res);
+        }).on('error', (error) => {
+        	next(error);
+       });
 	});
-
-	// // aarthy service call
-	// //https://fioridev1.dev.toyota.ca:44300/sap/opu/odata/sap/Z_VEHICLE_MASTER_SRV/zc_model
-	// //https://fioridev1.dev.toyota.ca:44300/sap/opu/odata/sap/Z_VEHICLE_MASTER_SRV/zc_model
-	// //https://nodemodule_pipeline.cfapps.us10.hana.ondemand.com/node/ZC_MODEL
-	// // https://sapdev.apimanagement.ca1.hana.ondemand.com:443/Z_VEHICLE_MASTER_SRV
-	// // https://sapdev.apimanagement.ca1.hana.ondemand.com:443/Z_VEHICLE_MASTER_SRV/zc_model
-	// /sap/opu/odata/sap/Z_VEHICLE_CATALOGUE_SRV/
-
-	app.get('/Z_VEHICLE_CATALOGUE_MODEL', function (req, res) {
-		//	var zcEntityset = req.param("zc_model");
-		var csrfToken;
-		console.log("req", req.param());
-		console.log("url", url);
-		console.log(url + "/Z_VEHICLE_CATALOGUE_SRV/");
-		request({
-			url: url +
-				"/Z_VEHICLE_CATALOGUE_SRV/zc_model/?$format=json",
-			headers: {
-				"Authorization": auth64,
-				"Content-Type": "application/json",
-				"APIKey": APIKey,
-				"x-csrf-token": "Fetch"
-			}
-		}, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				csrfToken = response.headers['x-csrf-token'];
-				// console.log(csrfToken);
-				var json = JSON.parse(body);
-				res.json(json);
-			} else {
-				var result = JSON.stringify(body);
-				res.type('application/json').status(401).send(result);
-			}
-		});
-	});
-	app.get('/Z_VEHICLE_CATALOGUE_MM', function (req, res) {
-		//	var zcEntityset = req.param("zc_model");
-		var csrfToken;
-		console.log(url);
-		console.log(url + "/Z_VEHICLE_CATALOGUE_SRV/");
-		request({
-			url: url +
-				"/Z_VEHICLE_CATALOGUE_SRV/zc_mmfields/?$format=json",
-			headers: {
-				"Authorization": auth64,
-				"Content-Type": "application/json",
-				"APIKey": APIKey,
-				"x-csrf-token": "Fetch"
-			}
-		}, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				csrfToken = response.headers['x-csrf-token'];
-				// console.log(csrfToken);
-				var json = JSON.parse(body);
-				res.json(json);
-			} else {
-				var result = JSON.stringify(body);
-				res.type('application/json').status(401).send(result);
-			}
-		});
-	});
-	app.get('/Z_VEHICLE_CATALOGUE_TRIM', function (req, res) {
-		//	var zcEntityset = req.param("zc_model");
-		var csrfToken;
-		console.log(url);
-		console.log(url + "/Z_VEHICLE_CATALOGUE_SRV/");
-		request({
-			url: url +
-				"/Z_VEHICLE_CATALOGUE_SRV/zc_exterior_trim/?$format=json",
-			headers: {
-				"Authorization": auth64,
-				"Content-Type": "application/json",
-				"APIKey": APIKey,
-				"x-csrf-token": "Fetch"
-			}
-		}, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				csrfToken = response.headers['x-csrf-token'];
-				// console.log(csrfToken);
-				var json = JSON.parse(body);
-				res.json(json);
-			} else {
-				var result = JSON.stringify(body);
-				res.type('application/json').status(401).send(result);
-			}
-		});
-	});
+	
 	return app;
 };
+
+
