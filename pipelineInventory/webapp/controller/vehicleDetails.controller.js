582fc9bff2c0f748db2e7ba7123ad20c8c351cd9
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-03-12 17:12:35 +0000

    commit- fixed pipeline to sold order navigation link as per updated url

--- pipelineInventory/webapp/controller/vehicleDetails.controller.js
+++ pipelineInventory/webapp/controller/vehicleDetails.controller.js
@@ -419,7 +419,9 @@ sap.ui.define([
 			var keys = "/" + modelyear + "/" + modelkey + "/" + serieskey + "/" + suffixkey + "/" + apxkey + "/" + colorkey + "/" + vtnn + "/" +
 				fromdate + "/" + todate + "/";
 				
-			window.location.href = "https://qa-soldorder.cfapps.us10.hana.ondemand.com/soldOrder/index.html#/page2" + keys;
+			window.location.href = "https://tci-qas-soldorderandpp.cfapps.us10.hana.ondemand.com/soldorderandpp/index.html#/page2" + keys;
+			//https://qa-soldorder.cfapps.us10.hana.ondemand.com/soldOrder/index.html#/page2
+			//https://tci-qas-soldorderandpp.cfapps.us10.hana.ondemand.com/soldorderandpp/index.html#/page2/2018/YZ3DCT/SIE/BB/00/0070/1234/20181212/20181224
 		},
 		onExit: function () {
 			this.destroy();
