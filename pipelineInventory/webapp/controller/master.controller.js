298822cf4eade526cb614377a1c9bb0ec03954f8
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-06-21 13:08:13 +0000

    commit- antional user series change

--- pipelineInventory/webapp/controller/master.controller.js
+++ pipelineInventory/webapp/controller/master.controller.js
@@ -387,9 +387,7 @@ sap.ui.define([
 							});
 						} else if (SelectedDealer === "2400500000" || SelectedDealer === "TCI Total") {
 							$.each(oModelData.d.results, function (key, value) {
-								// if (value.ModelSeriesNo !== "L/C") {
-								// 	delete oModelData.d.results[key];
-								// }
+								return oModelData.d.results[key];
 							});
 						} else {
 							$.each(oModelData.d.results, function (key, value) {
@@ -501,9 +499,7 @@ sap.ui.define([
 							});
 						} else if (SelectedDealer === "2400500000" || SelectedDealer === "TCI Total") {
 							$.each(oModelData.d.results, function (key, value) {
-								// if (value.ModelSeriesNo !== "L/C") {
-								// 	delete oModelData.d.results[key];
-								// }
+								return oModelData.d.results[key];
 							});
 						} else {
 							$.each(oModelData.d.results, function (key, value) {
