d1b33b3f36009a8f0329b69e628be083f6423526
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-06-21 14:44:49 +0000

    commit- change for national user series display

--- pipelineInventory/webapp/controller/master.controller.js
+++ pipelineInventory/webapp/controller/master.controller.js
@@ -379,19 +379,21 @@ sap.ui.define([
 								}
 							}
 						}
-						if (SelectedDealer !== "2400029000" && SelectedDealer !== "2400049000" && SelectedDealer !== "2400500000") {
+						if (SelectedDealer == "2400029000" && SelectedDealer == "2400049000") {
 							$.each(oModelData.d.results, function (key, value) {
-								if (value.ModelSeriesNo == "L/C") {
+								if (value.ModelSeriesNo !== "L/C") {
 									delete oModelData.d.results[key];
 								}
 							});
-						} else if (SelectedDealer === "2400500000" || SelectedDealer === "TCI Total") {
+						} 
+						// else if (SelectedDealer === "2400500000" && SelectedDealer === "TCI Total") {
+						// 	$.each(oModelData.d.results, function (key, value) {
+						// 		return oModelData.d.results[key];
+						// 	});
+						// } 
+						else if(SelectedDealer !== "2400029000" && SelectedDealer !== "2400049000" && SelectedDealer !== "2400500000" && SelectedDealer !== "TCI Total") {
 							$.each(oModelData.d.results, function (key, value) {
-								return oModelData.d.results[key];
-							});
-						} else {
-							$.each(oModelData.d.results, function (key, value) {
-								if (value.ModelSeriesNo !== "L/C") {
+								if (value.ModelSeriesNo === "L/C") {
 									delete oModelData.d.results[key];
 								}
 							});
@@ -491,19 +493,21 @@ sap.ui.define([
 					sap.ui.core.BusyIndicator.hide();
 					_that.oGlobalJSONModel.getData().seriesData = [];
 					if (oModelData.d.results.length > 0) {
-						if (SelectedDealer !== "2400029000" && SelectedDealer !== "2400049000" && SelectedDealer !== "2400500000") {
+						if (SelectedDealer == "2400029000" && SelectedDealer == "2400049000") {
 							$.each(oModelData.d.results, function (key, value) {
-								if (value.ModelSeriesNo == "L/C") {
+								if (value.ModelSeriesNo !== "L/C") {
 									delete oModelData.d.results[key];
 								}
 							});
-						} else if (SelectedDealer === "2400500000" || SelectedDealer === "TCI Total") {
+						} 
+						// else if (SelectedDealer === "2400500000" && SelectedDealer === "TCI Total") {
+						// 	$.each(oModelData.d.results, function (key, value) {
+						// 		return oModelData.d.results[key];
+						// 	});
+						// } 
+						else if(SelectedDealer !== "2400029000" && SelectedDealer !== "2400049000" && SelectedDealer !== "2400500000" && SelectedDealer !== "TCI Total") {
 							$.each(oModelData.d.results, function (key, value) {
-								return oModelData.d.results[key];
-							});
-						} else {
-							$.each(oModelData.d.results, function (key, value) {
-								if (value.ModelSeriesNo !== "L/C") {
+								if (value.ModelSeriesNo === "L/C") {
 									delete oModelData.d.results[key];
 								}
 							});
