daa75990b7210a6982b4f589e2c02c6d94bf075f
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-11-11 15:29:45 +0000

    fix for INC0154671

--- pipelineInventory_node/router/routes/user-details.js
+++ pipelineInventory_node/router/routes/user-details.js
@@ -142,7 +142,7 @@ module.exports = function (appContext) {
 							if (customerSalesArea.results[i].SalesOffice === bpZone) {
 								filtered = true;
 								if ((customerSalesArea.results[i].SalesOrganization == "6000" && customerSalesArea.results[i].DistributionChannel == "10" &&
-									customerSalesArea.results[i].Customer !== "2400500000") || (customerSalesArea.results[i].SalesGroup == "T99" && customerSalesArea.results[i].ProductAttribute1 == "X")) {
+									customerSalesArea.results[i].Customer !== "2400500000") && (customerSalesArea.results[i].SalesGroup == "T99" && customerSalesArea.results[i].ProductAttribute1 == "X")) {
 									resBody.sales.push(customerSalesArea.results[i]); //to fetch sales data
 								}
 							}
@@ -164,9 +164,9 @@ module.exports = function (appContext) {
 							if ((customerSalesArea.results[i].SalesOffice === "1000" || customerSalesArea.results[i].SalesOffice === "2000" ||
 									customerSalesArea.results[i].SalesOffice === "3000" || customerSalesArea.results[i].SalesOffice === "4000" ||
 									customerSalesArea.results[i].SalesOffice === "5000" || customerSalesArea.results[i].SalesOffice === "7000" ||
-									customerSalesArea.results[i].SalesOffice === "9000" || customerSalesArea.results[i].SalesOffice === "8000" || (customerSalesArea.results[i].SalesGroup == "T99" && customerSalesArea.results[i].ProductAttribute1 == "X")) && ((
+									customerSalesArea.results[i].SalesOffice === "9000" || customerSalesArea.results[i].SalesOffice === "8000") && ((
 									customerSalesArea.results[i].SalesOrganization == "6000") && (customerSalesArea.results[i].DistributionChannel == "10"
-									))) {
+									) && (customerSalesArea.results[i].SalesGroup == "T99" && customerSalesArea.results[i].ProductAttribute1 == "X"))) {
 								filtered = true;
 								resBody.sales.push(customerSalesArea.results[i]); //to fetch sales data
 							}
